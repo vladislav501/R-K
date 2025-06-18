9fcc4978732ff916cc5f82761ade09ed62670b28
daniil <tarima.949@gmail.com>
2025-06-18 11:58:25 +0300

    стили

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -44,7 +44,7 @@
                     <svg class="card-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                         <path d="M11.99 2C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2zM12 20c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8zm3.5-9c.83 0 1.5-.67 1.5-1.5S16.33 8 15.5 8 14 8.67 14 9.5s.67 1.5 1.5 1.5zm-7 0c.83 0 1.5-.67 1.5-1.5S9.33 8 8.5 8 7 8.67 7 9.5 7.67 11 8.5 11zm3.5 6.5c2.33 0 4.31-1.46 5.11-3.5H6.89c.8 2.04 2.78 3.5 5.11 3.5z"/>
                     </svg>
-                    <a href="">Управление типами одежды</a>
+                    <a href="{{ route('admin.clothing_types.index') }}">Управление типами одежды</a>
                 </div>
                 <div class="card">
                     <svg class="card-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
@@ -68,19 +68,19 @@
                     <svg class="card-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                         <path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z"/>
                     </svg>
-                    <a href="">Управление пунктами выдачи</a>
+                    <a href="{{ route('admin.pickup_points.index') }}">Управление пунктами выдачи</a>
                 </div>
                 <div class="card">
                     <svg class="card-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                         <path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z"/>
                     </svg>
-                    <a href="">Управление поставками</a>
+                    <a href="{{ route('admin.supplies.index') }}">Управление поставками</a>
                 </div>
                 <div class="card">
                     <svg class="card-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                         <path d="M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-2 10H7v-2h10v2z"/>
                     </svg>
-                    <a href="">Архив поставок</a>
+                    <a href="{{ route('admin.supplies.archive') }}">Архив поставок</a>
                 </div>
             </div>
         </div>
